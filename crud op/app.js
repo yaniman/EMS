a4e2dd4cd3e056d70593e7eeee6205dd65a76a30
yonas bekele <yonasbek4@gmail.com>
2022-01-13 17:19:59 +0300

    cors changes and few bug fixes

--- crud op/app.js
+++ crud op/app.js
@@ -2,9 +2,16 @@ const express = require("express")
 const mongoose = require("mongoose");
 const url = 'mongodb://127.0.0.1:27017/EMS'
 const app = express()
+var cors = require('cors')
+
 const bodyParser = require('body-parser');
 const add = require('./controller/employee');
 app.use(express.json());
+const corsOptions = {
+    origin: '*',
+}
+app.use(cors(corsOptions))
+
 app.use('/employee', add);
 
 mongoose.connect(url, { useNewUrlParser: true });
